ec39c0055ed3a82803fc4da5ba0e18b8b7e43db0
JerryDiaz <jdiaz2040@hotmail.com>
2019-09-14 20:46:47 -0600

    Se agrega codigo fuente

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interfaces';
+import { InfoEquipo } from '../interfaces/info-equipo.interfaces';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  equipoIfo: InfoEquipo[] = [];
+  cargada = false;
+  equipo: any[];
+
+  constructor(private http: HttpClient) { 
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo(){
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+         .subscribe( (resp: InfoPagina) => {
+           this.cargada = true;
+           this.info = resp;
+            //console.log(resp);
+         });
+  }
+
+//   private cargarEquipo(){
+//     // Leer el archivo JSON
+//     this.http.get('https://angular-html-8ab02.firebaseio.com/equipo.json')
+//          .subscribe( (resp: any[]) =>{
+//            this.equipo = resp;        
+//          } );
+//  }
+
+  private cargarEquipo(){
+      // Leer el archivo JSON
+     this.http.get('https://angular-html-8ab02.firebaseio.com/equipo.json')
+          .subscribe( (resp: InfoEquipo[]) =>{
+            this.equipoIfo = resp;            
+         } );
+   }
+
+}
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  productos: Producto[] = [];
+
+  constructor(private http: HttpClient) { 
+    this.cargarProducto();
+  }
+
+  private cargarProducto(){
+    this.http.get('https://angular-html-8ab02.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto[]) => {
+          this.productos = resp;
+          this.cargando = false;
+        } );    
+  }
+
+  getProducto( id: string ){
+    return this.http.get(`https://angular-html-8ab02.firebaseio.com/productos/${id}.json`);
+  }
+
+}
