c807c5c5e39fb1c87aa0f5dc23aa231c5864588a
JerryDiaz <jdiaz2040@hotmail.com>
2019-09-17 15:20:21 -0600

    Modificacion en el segmento

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductosService } from 'src/app/services/productos.service';
 import { Producto } from 'src/app/interfaces/producto.interface';
+import { HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-product',
@@ -11,15 +13,45 @@ export class ProductComponent implements OnInit {
 
   producto: Producto
 
+  public model = {
+    categoria: '1',
+    cod: '2',
+    titulo: '3',
+    url: '4'
+  };
+
   constructor(public productService: ProductosService) { }
 
   ngOnInit() {
   }
 
   save_product() {
-    if (this.producto.categoria == '') { return; }
+    if (this.model.categoria == '') { return; }
+    var json = {
+      "categoria": this.model.categoria,
+      "cod": this.model.cod,
+      "titulo": this.model.titulo,
+      "url": this.model.url
+    }
+    console.log(json);
+    this.productService.insert(json).subscribe(res => {
+      this.cleanModel();
+    },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log(err.message);
+        } else {
+          console.log(err.message);
+        }
+      });
 
-   
   }
 
+  cleanModel() {
+    this.model.categoria = '';
+    this.model.cod = '';
+    this.model.titulo = '';
+    this.model.url = '';
+
+  }
 }
